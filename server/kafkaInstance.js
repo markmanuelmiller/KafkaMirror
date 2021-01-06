ea0656e0d7cc77adb49d3d159cf66c176bce713e
Mark Miller <markmmiller825@gmail.com>
2021-01-06 14:22:15 -0700

    added default broker port 9092

--- server/kafkaInstance.js
+++ server/kafkaInstance.js
@@ -1,17 +1,17 @@
-const { Kafka } = require("kafkajs");
+const { Kafka } = require('kafkajs');
 
 // Default to inactive port, allows us to connect and pull information from multiple brokers if configured
-const createInstance = (brokers = 9090) => {
+const createInstance = (brokers = 9092) => {
   if (!Array.isArray(brokers)) brokers = [brokers];
   // Convert brokers input into correct format
   brokers = brokers.map((broker) => {
-    if (typeof broker === "number") return `localhost:${broker}`;
+    if (typeof broker === 'number') return `localhost:${broker}`;
     return broker;
   });
 
   return new Kafka({
     // Confirm clientId
-    clientId: "kafkamirror",
+    clientId: 'kafkamirror',
     brokers,
   });
 };
